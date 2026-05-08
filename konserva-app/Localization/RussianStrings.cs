78837ee2813e1fed07092b21491ac298480d4caa
vanja-san <vanjavs@mail.ru>
2026-05-08 17:44:16 +0700

    fix: add missing localization key SpawnSettings

--- konserva-app/Localization/RussianStrings.cs
+++ konserva-app/Localization/RussianStrings.cs
@@ -349,7 +349,7 @@ public static class RussianStrings
         { "ServerProperties_ManagementServerTlsKeystorePassword", "Пароль keystore TLS" },
         { "ServerProperties_ManagementServerTlsKeystorePassword_Desc", "Пароль для keystore TLS" },
         { "ServerProperties_Deprecated", "Устаревшие настройки" },
-        { "ServerProperties_AllowNether", "Доступ в Нижний мир" },
+        { "ServerProperties_SpawnSettings", "Спавн (устарев.)" },
         { "ServerDetail_Settings_General", "Название сервера" },
         { "ServerDetail_Settings_General_Desc", "Название сервера в приложении" },
         { "ServerDetail_Settings_Port", "Порт" },
